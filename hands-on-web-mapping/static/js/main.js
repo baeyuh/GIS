64491b2c5edefdf959dab330410c6495ae188ee2
Bea Remollo <sb.remollo@gmail.com>
2019-10-12 19:19:29 +0800

    Hands-on web mapping from https://bnhr.xyz/open-knowledge/hands-on-web-mapping/leaflet.html

---
+++ hands-on-web-mapping/static/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+var map = L.map('map', {
+    center: [12.8797, 121.7740],
+    zoom: 6,
+});
+
+var osm_mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {2AQVltc
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+})
